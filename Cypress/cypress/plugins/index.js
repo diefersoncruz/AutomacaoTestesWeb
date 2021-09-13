4d2f3b33e9784ec922985ccbf347cc22d600e1a6
Dieferson Cruz <dieferson.cruz@outlook.com>
2021-09-13 11:45:21 -0300

    Primeiro teste com Cypress

--- Cypress/cypress/plugins/index.js
+++ Cypress/cypress/plugins/index.js
@@ -1,6 +1,4 @@
 /// <reference types="cypress" />
-
-
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -22,11 +20,3 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
-
-
-const cucumber = require('cypress-cucumber-preprocessor').default
-module.exports = (on, config) => {
-  on('file:preprocessor', cucumber())
-}
-
-
